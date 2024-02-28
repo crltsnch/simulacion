59b4258067d5888a4de33eaffbe5edf396d86c08
crltsnch <crltsnch@myuax.com>
2024-02-28 18:10:50 +0100

    helmshoth

--- eliptica.py
+++ eliptica.py
@@ -14,30 +14,30 @@ d = float(input('Ingresar valor de d: '))
 # Cálculo de pasos
 h = (b - a) / N
 k = (d - c) / M
+l = float(input('Ingresar valor de l: '))
 
 # Inicialización de la matriz w con dimensiones correctas
 w = np.zeros((N+1, M+1))  # +1 para incluir los bordes
 
 # Función f(i, j) como fuente
 def f(i, j):
-    # Esta función debe ser actualizada según el problema específico
     return 0
-
-
+    # return 0 ecuacion de poisson
+    #return (a+h*i)*(c+k*j)*(1-a-h*i)*(1-c-k*j) 
+  
 for i in range(N):
     w[i][0] = 0  # Frontera inferior
-    w[i][M] =  0 #(a+h*i)**2 # Frontera superior/ #recordar que xi= xo(a) + ih
+    w[i][M] =  0  #(a+h*i)**2 Frontera superior, recordar que x_i= x_o(a) + ih
 
 for j in range(M):
-    w[0][j] =  0 #1-(c+k*j)**2  # Frontera izquierda
-    w[N][j] = 1 # Frontera derecha
+    w[0][j] =  0  #1-(c+k*j)**2  Frontera izquierda
+    w[N][j] = 1  # Frontera derecha
 
 # Iteraciones para la solución
 for iter in range(100):  # Número de iteraciones
     for i in range(1, N):
         for j in range(1, M):
-            w[i][j] = (k**2*(w[i+1][j]+w[i-1][j]) + h**2*(w[i][j+1]+w[i][j-1]) - 2*h**2*k**2*w[i][j]) / (2*(h**2 + k**2))
-
+            w[i][j] = (k**2*(w[i+1][j]+w[i-1][j]) + h**2*(w[i][j+1]+w[i][j-1]) - 2*h**2*k**2*f(i,j)) / (2*(h**2 + k**2) + h**2*k**2*(l**2))
 
 # Crear una malla de coordenadas para graficar
 x = np.linspace(a, b, N+1)
